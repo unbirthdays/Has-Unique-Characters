34c3a7c279b890eaa7d4a5f5881529699cb193d1
Sabrina Baccam <sabrinabaccam@gmail.com>
2022-09-12 02:29:48 -0500

    done

--- hasUniqueChars.js
+++ hasUniqueChars.js
@@ -1,1 +1,16 @@
 // Write your code below
+const hasUniqueChars = name => {
+    for (let i = 0; i < name.length; i++) {
+        for (let j = i + 1; j < name.length; j++) {
+            if (name[i] === name[j]) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+};
+
+console.log(hasUniqueChars("Monday"));
+
+console.log(hasUniqueChars("Moonday"));
